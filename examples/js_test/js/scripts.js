71ec975850b6364d8b4a5779d103be1b76b1e3f8
Mantas Zakas <mantas.zakas@gmail.com>
2019-05-23 23:00:49 +0300

    Homework 12 started

---
+++ examples/js_test/js/scripts.js
@@ -0,0 +1,16 @@
+alert("hello");
+function giveHelp() {
+    document.getElementById('shout').innerHTML = "<span style='color: red'>NOOO<span>";
+    // console for debugging
+    console.log("helped")
+
+    var myName = "Jonh";
+    var mySurname = "Sloth";
+    var age = 120;
+    var ageAfterNYears = age + 5;
+
+    console.log(ageAfterNYears);
+    console.log(myName + " " + mySurname);
+}
+
+
