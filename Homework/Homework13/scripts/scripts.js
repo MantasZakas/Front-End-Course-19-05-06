6a8039d6e36aca9f470b801df6b649066e9435f3
Mantas Zakas <mantas.zakas@gmail.com>
2019-05-28 08:35:53 +0300

    Homework 13 added scaling

--- Homework/Homework13/scripts/scripts.js
+++ Homework/Homework13/scripts/scripts.js
@@ -22,18 +22,28 @@ function diagonal (h, w) {
         return 0;
     }
 }
+function draw (h, w) {
+    var maxLength = 500; //scale the dimensions
+    if (h > w) {
+        var widthScaled = w * maxLength / h;
+        var heightScaled = maxLength;
+    } else {heightScaled = h * maxLength / w;
+        widthScaled = maxLength;
+    }
+    document.getElementById("rectangle").style.height = (heightScaled + "px"); //draw the rectangle
+    document.getElementById("rectangle").style.width = (widthScaled + "px");
+}
 function calculate (){
     var h = parseFloat(document.getElementById("height").value); //get value as number
     var w = parseFloat(document.getElementById("width").value);
-    var dimentions = [h, w];
-    var dimentionsChecked = dimentions.map(check); //check for negative numbers
-    if (dimentions[0] !== dimentionsChecked[0] || dimentions[1] !== dimentionsChecked[1]) {
+    var dimensions = [h, w];
+    var dimensionsChecked = dimensions.map(check); //check for negative numbers
+    if (dimensions[0] !== dimensionsChecked[0] || dimensions[1] !== dimensionsChecked[1]) {
         alert("Enter a valid number");
-        dimentionsChecked = [0, 0];
+        dimensionsChecked = [0, 0];
     }
-    document.getElementById("perimeter").innerHTML = perimeter (dimentionsChecked[0], dimentionsChecked[1]);
-    document.getElementById("area").innerHTML = area (dimentionsChecked[0], dimentionsChecked[1]);
-    document.getElementById("diagonal").innerHTML = diagonal (dimentionsChecked[0], dimentionsChecked[1]);
-    document.getElementById("rectangle").style.height = (h + "px"); //draw the rectangle
-    document.getElementById("rectangle").style.width = (w + "px");
+    document.getElementById("perimeter").innerHTML = perimeter (dimensionsChecked[0], dimensionsChecked[1]);
+    document.getElementById("area").innerHTML = area (dimensionsChecked[0], dimensionsChecked[1]);
+    document.getElementById("diagonal").innerHTML = diagonal (dimensionsChecked[0], dimensionsChecked[1]);
+    draw (dimensionsChecked[0], dimensionsChecked[1])
 }
